9fb7ffd0486a0f73a35a5a86d6a8b06199e5e1c1
Pastry Media <pastrymedia@gmail.com>
2013-10-15 19:35:30 -0400

    Added responsive features

--- library/extensions/responsive-viewport.php
+++ library/extensions/responsive-viewport.php
@@ -6,4 +6,24 @@ function hybrid_responsive_viewport () {
 }
 
 add_action('wp_head', 'hybrid_responsive_viewport', 1 );
+
+/* Wrap embeds with some custom HTML to handle responsive layout. */
+  add_filter( 'embed_handler_html', 'hybrid_embed_responsive_html' );
+  add_filter( 'embed_oembed_html',  'hybrid_embed_responsive_html' );
+
+/**
+ * Wraps embeds with <div class="embed-wrap"> to help in making videos responsive.
+ *
+ * @since  0.1.0
+ * @access public
+ * @return void
+ */
+function hybrid_embed_responsive_html( $html ) {
+
+  if ( in_the_loop() && has_post_format( 'video' ) && preg_match( '/(<embed|object|iframe)/', $html ) )
+    $html = '<div class="embed-wrap">' . $html . '</div>';
+
+  return $html;
+}
+
 ?>
